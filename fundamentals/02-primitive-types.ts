4bc1170d547c3f0752c025a4fc63a33e6251f418
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-12 13:03:36 +0100

    primitive types

---
+++ fundamentals/02-primitive-types.ts
@@ -0,0 +1,23 @@
+
+//primitive types: number
+const lessonsCount = 10;
+const total = lessonsCount + 10;
+console.log("total =", total)
+
+//primitive types: string
+let title = "Typescript BootCamp";
+
+let subTitle = 'Learn the language fundamentals, build practical projects';
+
+const fullTitle = title + ": " + subTitle;
+
+console.log("Full title: " + fullTitle);
+
+
+//primitive types: boolean
+
+const published = false;
+
+if(published){
+    console.log("The course is published");
+}
