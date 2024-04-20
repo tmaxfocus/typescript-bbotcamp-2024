032662e06f1f682eccda6ecc431331a4a9ad5d8a
Tobi Adeogun <Tobi.Adeogun@stanbicibtc.com>
2024-04-20 01:08:51 +0100

    string template

---
+++ fundamentals/03-template-strings.js
@@ -0,0 +1,5 @@
+// template strings
+var _title = "Typescript BootCamp";
+var _subTitle = "Learn the language fundamentals, build practical project";
+var _fullTitle = "Full title: ".concat(_title, ": ").concat(_subTitle);
+console.log("Full title: " + _fullTitle);
---
+++ fundamentals/03-template-strings.ts
@@ -0,0 +1,8 @@
+// template strings
+let _title = "Typescript BootCamp";
+
+let _subTitle = "Learn the language fundamentals, build practical project"
+
+const _fullTitle = `Full title: ${_title}: ${_subTitle}`;
+
+console.log("Full title: " + _fullTitle);
